139083c9b7affc3fb655da732282efedaa4b68f3
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2013-09-29 17:03:08 -0700

    Rspec tests up and running

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,55 +1,38 @@
+$LOAD_PATH.unshift(File.expand_path('./app'))
 ENV['RACK_ENV'] = 'test'
-require '../controller'
-require 'test/unit'
 require 'rack/test'
-require_relative '../app/models/pack'
-require_relative "../app/models/wolf"
+require_relative '../db/config'
+require_relative '../controller'
 
 
-class IndexTest < Test::Unit::TestCase
+describe "new user core capabilities" do
   include Rack::Test::Methods
 
-  def app
+  def app 
     Sinatra::Application
   end
 
-  def test_it_creates_a_wolf
-    post '/new_game', params={name: "Test_Wolf"}
-    assert last_response.ok?
-    assert_equal 'Hello World', last_response.body
-  end
-
-  def test_it_creates_a_pack
-    get '/', :name => 'Simon'
-    assert last_response.body.include?('Simon')
-  end
-
-  def test_it_sets_new_wolf_pack_to_new_pack
-  end
-end
-
-describe "new user core capabilities" do
-  before(:each) do
-    wolf = Wolf.create(name: "Test_Wolf")
-    pack = Pack.create(name: "Test_Pack")
+  before(:all) do
+    @wolf = Wolf.create(name: "Test_Wolf")
+    @pack = Pack.create(name: "Test_Pack")
   end
 
   it "creates new wolf" do
-   wolf.name === "Test_Wolf"
+    @wolf.name === "Test_Wolf"
   end
 
   it "creates new pack" do
-    pack.name === "Test_Pack"
+    @pack.name === "Test_Pack"
   end
 
   it "places wolf in new pack" do
-    wolf.pack = pack
-    wolf.save
-    wolf.pack_id == pack.id
+    @wolf.pack = @pack
+    @wolf.save
+    @wolf.pack_id == @pack.id
   end
 
-  after(:each) do
-    Wolf.delete(wolf.id)
-    Pack.delete(pack.id)
+  after(:all) do
+    Wolf.delete_all
+    Pack.delete_all
   end
 end
