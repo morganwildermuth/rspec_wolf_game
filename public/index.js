404f5c515b91332db70b358f95a8174fba843937
Morgan Wildermuth <WEF6@Morgans-MacBook-Air.local>
2013-10-01 21:46:02 -0700

    remove facebook oauth

--- public/index.js
+++ public/index.js
@@ -1,38 +1,3 @@
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '414640511968824', // App ID
-    channelUrl : '//wolf-sim.herokuapp.com/channel.html', // Channel File
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
-
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    if (response.status === 'connected') {
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      FB.login();
-    } else {
-      FB.login();
-    }
-  });
-  };
-
-  (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
-
 bindListeners = function() {
   document.getElementById("newGameForm").addEventListener("click", alertNewGame);
 }
